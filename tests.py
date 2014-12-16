7a44066306b4c9d35de8bf881d9a7a1ee058f1be
rmaratos <rmaratos@andrew.cmu.edu>
2014-12-15 21:38:36 -0500

    Learning unittests

---
+++ tests.py
@@ -0,0 +1,23 @@
+"""Tests for User, Network, and Infection"""
+import unittest
+
+
+class UserTests(unittest.TestCase):
+    """Tests for User Class"""
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
+class NetworkTests(unittest.TestCase):
+    """Tests for Network and Group Class"""
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
+class InfectionTests(unittest.TestCase):
+    """Tests for Infection Class"""
+    def test_something(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
